5561339da603e8765a053bb01b7467c6f2679468
David Duncker <Dave.Duncker@gmail.com>
2016-08-06 07:32:22 -0400

    Update county-level-general-election-scanner.py
    Cleaned up code, made it executable.

--- county-level-general-election-scanner.py
+++ county-level-general-election-scanner.py
@@ -1,36 +1,40 @@
-from bs4 import BeautifulSoup as bs
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup as HTMLparser
 import requests
 import time
 import MySQLdb
-def scan():
-	invalid_numbers=[]
-	for year in [2000,2004,2008,2012]:
-		for state_number in range(1,100):
+def scanWebForElectionData():
+	invalidStateNumbers=[]
+	listOfElectionYears=[2000,2004,2008,2012]
+	for year in listOfElectionYears:
+		for numberCorrespondingToState in range(1,100):
 			try:
 				time.sleep(5)
-				r=requests.get('http://uselectionatlas.org/RESULTS/datagraph.php?fips='+str(state_number)+'&year='+str(year))
-				soup=bs(r._content,"html.parser")
-				q=soup.find("div",class_="header")
-				state=q.contents[0].split(' ')[7]
-		
-				w=soup.find("div",class_="info").find_all("table")
-				for i in range(0,len(w)):
-					county=w[i].tr.td.b.contents[0]
-					q=w[i].find_all("tr")
-					for j in range(0,len(q)):
+				HTMLrequest=requests.get('http://uselectionatlas.org/RESULTS/datagraph.php?fips='+str(numberCorrespondingToState)+'&year='+str(year))
+				parsedHTML=HTMLparser(HTMLrequest._content,"html.parser")
+				parsedHTMLwithStateName=parsedHTML.find("div",class_="header")
+				stateName=parsedHTMLwithStateName.contents[0].split(' ')[7]
+				messyListOfInfo=parsedHTML.find("div",class_="info").find_all("table")
+				for i in range(0,len(messyListOfInfo)):
+					countyName=messyListOfInfo[i].tr.td.b.contents[0]
+					messyListOfCandidates=messyListOfInfo[i].find_all("tr")
+					for j in range(0,len(messyListOfCandidates)):
 						if j==0:
-							candidate=w[i].find_all("tr")[j].find_all("td")[1].contents[0]
-							vote_perc=w[i].find_all("tr")[j].find_all("td")[2].contents[0]
-							vote_total=w[i].find_all("tr")[j].find_all("td")[3].contents[0]
+							candidateName=messyListOfInfo[i].find_all("tr")[j].find_all("td")[1].contents[0]
+							votePercentage=messyListOfInfo[i].find_all("tr")[j].find_all("td")[2].contents[0]
+							voteTotal=messyListOfInfo[i].find_all("tr")[j].find_all("td")[3].contents[0]
 						else:
-							candidate=w[i].find_all("tr")[j].find_all("td")[0].contents[0]
-							vote_perc=w[i].find_all("tr")[j].find_all("td")[1].contents[0]
-							vote_total=w[i].find_all("tr")[j].find_all("td")[2].contents[0]
-						print str(year) + "\t" + state + " " + str(state_number) + "\t" + county + "\t" + candidate + "\t" + vote_perc + "\t" + vote_total
+							candidateName=messyListOfInfo[i].find_all("tr")[j].find_all("td")[0].contents[0]
+							votePercentage=messyListOfInfo[i].find_all("tr")[j].find_all("td")[1].contents[0]
+							voteTotal=messyListOfInfo[i].find_all("tr")[j].find_all("td")[2].contents[0]
+						print str(year) + "\t" + stateName + " " + str(numberCorrespondingToState) + "\t" + countyName + "\t" + candidateName + "\t" + votePercentage + "\t" + voteTotal
 					print "\n"
 				print "\n\n"
 			except AttributeError:
-				print "State number " + str(state_number) + " is invalid"
-				invalid_numbers.append(state_number)
+				print "State number " + str(numberCorrespondingToState) + " is invalid"
+				invalidStateNumbers.append(numberCorrespondingToState)
 
+if __name__ == "__main__":
+	scanWebForElectionData()
 
